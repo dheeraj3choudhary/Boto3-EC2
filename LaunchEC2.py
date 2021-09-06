0e153b0f159ea73a77b9bede178a460514d4cfcc
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2021-09-06 15:24:48 +0530

    Update LaunchEC2.py

--- LaunchEC2.py
+++ LaunchEC2.py
@@ -2,10 +2,11 @@ import boto3
 
 client = boto3.client('ec2')
 
+#Please select image id as per your EC2 instance requirement
 resp=client.run_instances(ImageId='ami-0742b4e673072066f',
                           InstanceType='t2.micro',
                           MinCount=1,
                           MaxCount=1,
-                          KeyName='CustomVPC')
+                          KeyName='<keyname>') #Enter your key name here
 for i in resp['Instances']:
     print("Instance ID Created is :{} Instance Type Created is : {}" .format(i['InstanceId'],i['InstanceType']))
