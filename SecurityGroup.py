124c937d89137bf4d71da8015123698535802d13
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2021-10-26 17:56:30 +0530

    Security group
    Python script to create security group and add ingress rules

---
+++ SecurityGroup.py
@@ -0,0 +1,32 @@
+import boto3
+
+client = boto3.client('ec2')
+
+# Create a security group and allow SSH inbound rule through the VPC
+security_resp = client.create_security_group(GroupName='Inspector-SG',
+                                             Description='Security group for aws Inspector',
+                                             VpcId='vpc-78713102')
+security_group_id = security_resp['GroupId']
+print(security_group_id)
+
+sgrule_ingress = client.authorize_security_group_ingress(
+        GroupId=security_group_id,
+        IpPermissions=[
+            {'IpProtocol': 'tcp',
+             'FromPort': 22,
+             'ToPort': 22,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+            {'IpProtocol': 'tcp',
+             'FromPort': 20,
+             'ToPort': 20,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+            {'IpProtocol': 'tcp',
+             'FromPort': 21,
+             'ToPort': 21,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+            {'IpProtocol': 'tcp',
+             'FromPort': 23,
+             'ToPort': 23,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+
+        ])
