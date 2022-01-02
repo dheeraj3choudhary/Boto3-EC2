e2e23eb00dcd6ff135b0f72334d9aa581c892b0b
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2022-01-02 14:28:35 +0530

    Enable list of cloudwatch alarms

---
+++ Enable_Cloudwatch_Alarms.py
@@ -0,0 +1,10 @@
+###Author :- Dheeraj Choudhary
+import boto3
+
+client = boto3.client('cloudwatch')
+
+file1 = open("D:\Python Impetus\AWS_Boto3\myfile.txt", "r")
+enable_alarm = file1.readlines()
+enable_alarm = [line.rstrip() for line in enable_alarm]
+print(enable_alarm)
+response = client.enable_alarm_actions(AlarmNames=enable_alarm)
