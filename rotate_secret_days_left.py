09a2e1f209af695d07be5d01ba5a98139ec26be8
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2022-02-27 20:09:11 +0530

    Update rotate_secret_days_left.py

--- rotate_secret_days_left.py
+++ rotate_secret_days_left.py
@@ -1,23 +1,24 @@
 # @Author:- Dheeraj Choudhary
 import boto3
-import pprint
 from datetime import datetime
 from datetime import timedelta
 from dateutil.tz import tzlocal
-
 client = boto3.client('secretsmanager')
+
 list_sec = client.list_secrets()
 for i in list_sec['SecretList']:
-    rotation_status = 'RotationEnabled' in i
-    #check = 'LastRotatedDate' in i
-    if rotation_status:
+    rotation_status = (i['RotationEnabled']) if 'RotationEnabled' in i else 'False'
+    rotation_date = ('LastRotatedDate' in i)
+    rotation_date = str(rotation_date)
+    print(f"rotation_status = {rotation_status}{type(rotation_status)} And rotation_date = {rotation_date}{type(rotation_date)}")
+    if (rotation_status == 'True') & (rotation_date == 'True'):
         response = client.describe_secret(SecretId=i['ARN'])
-        LastChangedDate = response['LastChangedDate']
+        #LastChangedDate = response['LastChangedDate']
+        Last_Rotated = response['LastRotatedDate']
         AutomaticallyAfterDays = response['RotationRules']['AutomaticallyAfterDays']
-        total = LastChangedDate + timedelta(days=AutomaticallyAfterDays)
+        total = Last_Rotated + timedelta(days=AutomaticallyAfterDays)
         current_timestamp = datetime.now(tzlocal())
         days_left = total - current_timestamp
-        print(f"Days left for secret name '{response['Name']}' to be rotated = {days_left.days}")
-
-
+        if days_left.days <= 15:
+            print(f"Days left for secret name '{response['Name']}' to be rotated = {days_left.days}")
 
